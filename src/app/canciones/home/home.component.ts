62d37671187bb7ff8eb7d809021952bd50ee19fa
fiorelath <ftorresherrera874@gmail.com>
2025-06-25 18:22:51 -0500

    Visualización de canciones implementada en HomeComponent

--- src/app/canciones/home/home.component.ts
+++ src/app/canciones/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CancionesService, Cancion } from 'src/app/servicios/canciones.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  canciones$: Observable<Cancion[]> | undefined;
 
+  constructor(private cancionesService: CancionesService) {}
+
+  ngOnInit(): void {
+    this.canciones$ = this.cancionesService.obtenerCanciones();
+  }
 }
