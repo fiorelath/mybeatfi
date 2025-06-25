9c3abb08905910c1e7fd879af4faaf4d0d717984
fiorelath <ftorresherrera874@gmail.com>
2025-06-24 22:07:34 -0500

    Servicios actualizados con conexión a Firestore

--- src/app/servicios/canciones.service.ts
+++ src/app/servicios/canciones.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+export interface Cancion {
+  id?: string;
+  titulo: string;
+  artista: string;
+  genero: string;
+  duracion: number;
+  imagenUrl: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CancionesService {
 
-  constructor() { }
+  constructor(private firestore: Firestore) {}
+
+  obtenerCanciones(): Observable<Cancion[]> {
+    const cancionesRef = collection(this.firestore, 'canciones');
+    return collectionData(cancionesRef, { idField: 'id' }) as Observable<Cancion[]>;
+  }
 }
--- src/app/servicios/playlists.service.ts
+++ src/app/servicios/playlists.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+export interface Playlist {
+  id?: string;
+  nombre: string;
+  descripcion: string;
+  canciones: string[]; // IDs de canciones
+  uid: string; // ID del usuario que la creó
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlaylistsService {
 
-  constructor() { }
+  constructor(private firestore: Firestore) {}
+
+  obtenerPlaylists(): Observable<Playlist[]> {
+    const playlistsRef = collection(this.firestore, 'playlists');
+    return collectionData(playlistsRef, { idField: 'id' }) as Observable<Playlist[]>;
+  }
 }
